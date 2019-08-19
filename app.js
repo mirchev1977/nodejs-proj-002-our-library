e913b3b20ad723116a438ce0792e4559ee9f54bb
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-19 21:41:53 +0300

    Welcome to our Library

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ const app = express();
 
 app.use( bodyParser.urlencoded( { extended: false } ) );
 
+app.get( '/', ( req, res, next ) => {
+    res.status( 200 ).setHeader( 'Content-Type', 'text/html' );
+    res.write( '<h1>Welcome to our Library!</h1>' );
+    res.send();
+} );
+
 app.listen( 3000 );
