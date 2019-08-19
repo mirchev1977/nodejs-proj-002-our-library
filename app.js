cb63b89105e2cb5697c2664ed923bef53e143b41
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-19 21:49:56 +0300

    created router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express    = require( 'express'     );
 const bodyParser = require( 'body-parser' );
 
 const app = express();
+const router = express.Router();
 
 app.use( bodyParser.urlencoded( { extended: false } ) );
 
