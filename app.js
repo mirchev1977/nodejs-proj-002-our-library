1bfa8f541f51b17640c05b6a6b96da1fab3fff8d
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-19 21:27:10 +0300

    installed and included body-parser

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const express = require( 'express' );
+const express    = require( 'express'     );
+const bodyParser = require( 'body-parser' );
 
 const app = express();
+
+app.use( bodyParser.urlencoded( { extended: false } ) );
 
 app.listen( 3000 );
