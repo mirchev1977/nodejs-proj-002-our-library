6eb5d009629b768c5bdc713079c370070aada29d
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-21 04:42:13 +0300

    read the books from ./data/books.txt

--- routes/library.js
+++ routes/library.js
@@ -1,3 +1,4 @@
+const fs      = require( 'fs'      );
 const express = require( 'express' );
 
 const t = require( '../utils/templates' );
@@ -6,14 +7,26 @@ const router = express.Router();
 
 
 router.get( '', ( req, res, next ) => {
-    t( 'homepage', {
-        one: 'one-value',
-        two: 'two-value',
-        arr: [ 'title;author;issuedon','one;two;three', 'four;five;six' ]
-    } ).then( html => {
-        res.status( 200 ).setHeader( 'Content-Type', 'text/html' );
-        res.write( html );
-        res.end();
+    let fileData = '';
+    fs.readFile( './data/books.txt', 'utf-8', ( err, data ) => {
+        if ( err ) {
+            debugger;
+        }
+
+        fileData += data;
+
+        const arrData = fileData.split( /\n/ );
+        arrData.pop();
+
+        t( 'homepage', {
+            one: 'one-value',
+            two: 'two-value',
+            arr: arrData 
+        } ).then( html => {
+            res.status( 200 ).setHeader( 'Content-Type', 'text/html' );
+            res.write( html );
+            res.end();
+        } );
     } );
 } );
 
