9eea484f1be1332eb4d9a525635954c7ca44c66d
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-22 05:48:07 +0300

    set public as a static files dir

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express    = require( 'express'           );
 const bodyParser = require( 'body-parser'       );
+const path       = require( 'path'              );
+
+const rootDir = require( './utils/path' );
 
 const routesLibrary = require( './routes/library' );
 const routesAdmin   = require( './routes/admin'   );
 
 const app = express();
+
+app.use( express.static( path.join( rootDir, 'public' ) ) );
 
 app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( '/',      routesLibrary );
